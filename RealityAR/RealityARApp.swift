8adb767be0c01e4e8aae3ac7ac4249a259938750
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-03 14:41:21 +0200

    Initial commit

---
+++ RealityAR/RealityARApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityARApp.swift
+//  RealityAR
+//
+//  Created by Davide Castaldi on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RealityARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
